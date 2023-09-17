0bd2bb6e320ec486d2555391f938b45f9a30fb39
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-09-17 16:32:27 +0200

    feat: install packages and missing files

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const config = {
+  solidity: "0.8.18",
+  networks: {},
+};
+
+// Check network to use
+if (process.env.SEPOLIA_URL_PROVIDER && process.env.SEPOLIA_URL_PROVIDER != "")
+  config.networks.sepolia = {
+    url: process.env.SEPOLIA_URL_PROVIDER,
+    accounts: [process.env.PRIVATE_KEY],
+  };
+if (process.env.MAINNET_URL_PROVIDER && process.env.MAINNET_URL_PROVIDER != "")
+  config.networks.mainnet = {
+    url: process.env.MAINNET_URL_PROVIDER,
+  };
+
+// Etherscan apikey
+if (process.env.ETHERSCAN_API_KEY && process.env.ETHERSCAN_API_KEY) {
+  config.etherscan = {};
+  config.etherscan.apiKey = process.env.ETHERSCAN_API_KEY;
+}
+
+module.exports = config;
