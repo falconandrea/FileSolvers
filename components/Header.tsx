cf58c9505bda7bb87fd86ac390c4ec4c7050cd27
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-09-23 18:46:48 +0200

    chore: change label

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
           title="All the requests"
           className="text-gray-800 hover:underline mr-8"
         >
-          All
+          List
         </Link>
       </div>
     </header>
